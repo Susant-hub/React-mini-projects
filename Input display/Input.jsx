6b2f16f3df7efbedc2077db15fdf26f731a77dae
Tech_sus <115480300+Susant-hub@users.noreply.github.com>
2025-05-18 00:32:11 +0545

    Create Input.jsx

---
+++ Input display/Input.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import {useState} from 'react'
+
+function Input() {
+    const [message, setMessage]=useState("Whatever you type will appear down here")
+  return (
+    <div>
+      <form>
+        <input type='text' value={message} onChange={(evnt)=>setMessage(evnt.target.value)}></input>
+      </form>
+      <h1>{message}</h1>
+    </div>
+  )
+}
+export default Input
