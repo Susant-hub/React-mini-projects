22a345791b274787c5c2e7ff646b79744c1bf6bd
Tech_sus <115480300+Susant-hub@users.noreply.github.com>
2025-05-22 00:00:16 +0545

    Update Completed as well as Delete button with more proper functionality and stylings
    -Added Completed button
    -Added  more styling on clicking the button

--- React-Todo-App/Todo.jsx
+++ React-Todo-App/Todo.jsx
@@ -7,16 +7,36 @@ import { FaCheck } from "react-icons/fa6";
 function Input() {
   const [message, setMessage]=useState("")
   const[todos, setTodos]=useState([])
+  const [doneIndex, setDoneIndex] = useState()
+  const [deleteIndex, setDeleteIndex]=useState()
+
 
   function handleTodo(e){
     e.preventDefault()
+    if(message=="") return
+    if(todos.includes(message))return
     setTodos((prev)=>[...prev, message])
     setMessage("")
   }
 
-  function handleDelete(indexToRemove){
-    setTodos(prev => prev.filter((_, idx) => idx !== indexToRemove));
+  function handleDone(indexToDone) {
+  setDoneIndex(indexToDone);
+
+  setTimeout(() => {
+    setTodos(prev => prev.filter((_, index) => index !== indexToDone));
+    setDoneIndex();
+  }, 1000);
+}
+
+  function handleDelete(indexToDelete){
+    setDeleteIndex(indexToDelete)
+
+    setTimeout(()=>{
+      setTodos((prev)=>prev.filter((_,index)=>index!==indexToDelete))
+      setDeleteIndex()
+    },1000)
   }
+
   return (
     <div className='container'>
       <h1 className='heading'>Todo App</h1>
@@ -26,11 +46,19 @@ function Input() {
       </form>
       <div className='tasks'>
         {todos.map((ele, index)=>
-            <div key={index} className='task-container'>
-              <div  className='task'>{ele}</div>
-              <div className='icons'><MdDelete  onClick={()=>handleDelete(index)} className='delete'/></div>
+            <div key={index} className={`task-container ${doneIndex===index ?"done-task" :""} ${deleteIndex===index ?"delete-task" : ""}`}>
+              <div  className='task'>{doneIndex===index ? "Task Completed" : deleteIndex===index ?"Task Removed" : ele} </div>
+              <div className='icons'>
+                {(doneIndex===index || deleteIndex===index) ?"":
+                <>
+                <FaCheck className='done' onClick={()=>handleDone(index)} />
+                <MdDelete  onClick={()=>handleDelete(index)} className='delete'/>
+                </>
+                }
+              </div>
             </div>)}
         </div>
+       {todos.length==0?"":<div className="btn-2"><button onClick={(()=>setTodos([]))} className='clear-btn'>Clear All</button></div>} 
     </div>
   )
 }
