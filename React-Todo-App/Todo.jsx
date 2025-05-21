758bc6f11c12093f759d66f9f3fa96e91c15ef91
Tech_sus <115480300+Susant-hub@users.noreply.github.com>
2025-05-21 19:49:45 +0545

    Update Todo app with delete functionality and added css styling
    - Users can add tasks via input field
    - Tasks are displayed in a list
    - Each task has a delete button to remove it
    - Used useState for state management
    - Styling added using CSS

--- React-Todo-App/Todo.jsx
+++ React-Todo-App/Todo.jsx
@@ -1,4 +1,8 @@
 import React, {useState} from 'react'
+import "./Input.css"
+import { MdAdd } from "react-icons/md";
+import { MdDelete } from "react-icons/md";
+import { FaCheck } from "react-icons/fa6";
 
 function Input() {
   const [message, setMessage]=useState("")
@@ -6,18 +10,27 @@ function Input() {
 
   function handleTodo(e){
     e.preventDefault()
-   setTodos((prev)=>[...prev, message])
-   setMessage("")
+    setTodos((prev)=>[...prev, message])
+    setMessage("")
+  }
+
+  function handleDelete(indexToRemove){
+    setTodos(prev => prev.filter((_, idx) => idx !== indexToRemove));
   }
   return (
-    <div>
-      <form>
-        <input type="text" value={message} onChange={(evnt)=>{setMessage(evnt.target.value)}}></input>
-        <button onClick={handleTodo}>Add</button>
+    <div className='container'>
+      <h1 className='heading'>Todo App</h1>
+      <form className="form">
+        <input type="text" value={message} onChange={(evnt)=>{setMessage(evnt.target.value)}} className='input' placeholder="Enter your tasks"></input>
+        <button onClick={handleTodo} className='btn'><MdAdd/></button>
       </form>
-      <ol>
-        {todos.map((ele, index)=><li key={index}>{ele}</li>)}
-      </ol>
+      <div className='tasks'>
+        {todos.map((ele, index)=>
+            <div key={index} className='task-container'>
+              <div  className='task'>{ele}</div>
+              <div className='icons'><MdDelete  onClick={()=>handleDelete(index)} className='delete'/></div>
+            </div>)}
+        </div>
     </div>
   )
 }
