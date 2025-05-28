0e35e2998d728da3d3bb877104451d8b1998e447
Tech_sus <115480300+Susant-hub@users.noreply.github.com>
2025-05-28 23:30:32 +0545

    Update Counter.jsx
    added steps input to not only increase or decrease by one but with the number user provides.

--- Number counter/Counter.jsx
+++ Number counter/Counter.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import {useState} from 'react'
+import { MdMargin } from 'react-icons/md';
 
 function Counter() {
     let [count, setCount] = useState(0)
+    let [steps, setSteps] = useState(0)
 
     const divStyling={
         display:"flex",
         flexDirection:"column",
         alignItems:"center",
-        fontSize:"50px"
     };
     const buttons={
         width:"75px",
@@ -18,15 +19,20 @@ function Counter() {
     }
     const buttonStylings={
         display:"flex",
-        gap:"10px"
+        gap:"10px",
+        marginTop:"20px"
     };
     
   return (
     <div style={divStyling}>
-      <h1>{count}</h1>
+      <h1 style={{fontSize:"60px"}}>{count}</h1>
+      <p>Steps to increase or decrease the count:</p>
+
+      <input type='number' onChange={(e)=>setSteps(Number(e.target.value))}></input>
+
       <div style={buttonStylings}>
-      <button style={buttons} onClick={()=>setCount((prev)=> {return (prev+1)})}>Plus</button>
-      <button style={buttons} onClick={()=>setCount((prev)=>{return (prev-1)})}>Minus</button>
+      <button style={buttons} onClick={()=>setCount((prev)=> {return (prev+steps)})}>Plus</button>
+      <button style={buttons} onClick={()=>setCount((prev)=>{return (prev-steps)})}>Minus</button>
       <button style={buttons} onClick={()=>setCount(0)}>Restart</button>
       </div>
     </div>
