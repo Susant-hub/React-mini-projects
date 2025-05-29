bb8a82384202fd758e7c92537c91078b402c07a3
Tech_sus <115480300+Susant-hub@users.noreply.github.com>
2025-05-30 00:02:13 +0545

    Update RegistrationForm.jsx
    Added an object state fro all input fields
    
    destructured name and value from event.target
    
    updated the object property by spread operator {...}
    
    [ ] in name means dynamic ; doesnt't adds new object property
    if already existed in the object, changes the value just

--- Registration-form/RegistrationForm.jsx
+++ Registration-form/RegistrationForm.jsx
@@ -2,36 +2,45 @@ import React,{useState} from 'react'
 
 function Registration() {
 
-    const [firstName, setFirstName] = useState("")
-    const[lastName, setLastName]= useState("")
-    const[email, setEmail]= useState("")
-    const[password, setPassword]= useState("") 
-    const[number, setNumber]= useState("")
+    const [user, setUser] = useState(
+        {
+            firstName:"",     
+            lastName:"",
+            email:"",
+            password:"",         
+            number:""     
+        }
+    )  
+
+    function handleInput(e){
+        const {name, value}=e.target        // destructuring the event.target from the event onChange
+
+        setUser((prev)=> ({...prev, [name]:value}))          // [ ] in name means dynamic ; doesnt't adds new object property
+                                                            // if already existed in the object, changes the value just
+    }
+    function handleClick(){
+        console.log(user)
+    }
 
   return (
-
-    <div>
+    <div style={{textAlign:"center"}}>
       <p >First Name:</p>
-      <input type="text" value={firstName} onChange={(e)=>setFirstName(e.target.value)}></input>
+      <input type="text" value={user.firstName} name='firstName' onChange={(e)=>handleInput(e)}></input>
 
       <p >Last Name:</p>
-      <input type="text" value={lastName} onChange={(e)=>setLastName(e.target.value)}></input>
+      <input type="text" value={user.lastName} name='lastName' onChange={(e)=>handleInput(e)}></input>
 
       <p >Email:</p>
-      <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)}></input>
+      <input type="text" value={user.email} name='email' onChange={(e)=>handleInput(e)}></input>
 
       <p >Password:</p>
-      <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)}></input>
+      <input type="text" value={user.password} name='password' onChange={(e)=>handleInput(e)}></input>
 
       <p >Number:</p>
-      <input type="text" value={number} onChange={(e)=>setNumber(e.target.value)}></input>
+      <input type="text" value={user.number} name='number' onChange={(e)=>handleInput(e)}></input>
 
-      <h2>Information:</h2>
-      {firstName && lastName && <h3>Name: {firstName} {lastName}</h3>}
-      {email && <h3>Email: {email}</h3>}
-      {password && <h3>Password: {password}</h3>}
-      {number && <h3>Number: {number}</h3>}
-
+      <button onClick={handleClick} style={{display:"block", margin:"auto", marginTop:"20px"}}>Sign Up</button>
+      
     </div>
   )
 }
